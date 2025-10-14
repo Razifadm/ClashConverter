9e29a91cf0ac900ad28278f4260832aea7ae4bd1
raducksijaa <ronald.alan77@gmail.com>
2025-10-14 14:01:01 +0800

    Update Install.sh-silent

--- Install.sh
+++ Install.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
+echo "installing Clash Converter...."
+
 # Buat folder kalau tak wujud
-mkdir -p /usr/lib/lua/luci/controller
-mkdir -p /usr/lib/lua/luci/view/clash_converter
+mkdir -p /usr/lib/lua/luci/controller >/dev/null 2>&1
+mkdir -p /usr/lib/lua/luci/view/clash_converter >/dev/null 2>&1
 
 # Download controller
 wget -O /usr/lib/lua/luci/controller/clash_converter.lua \
-https://raw.githubusercontent.com/Razifadm/ClashConverter/main/usr/lib/lua/luci/controller/clash_converter.lua
+https://raw.githubusercontent.com/Razifadm/ClashConverter/main/usr/lib/lua/luci/controller/clash_converter.lua >/dev/null 2>&1
 
 # Download view
 wget -O /usr/lib/lua/luci/view/clash_converter/index.htm \
-https://raw.githubusercontent.com/Razifadm/ClashConverter/main/usr/lib/lua/luci/view/clash_converter/index.htm
+https://raw.githubusercontent.com/Razifadm/ClashConverter/main/usr/lib/lua/luci/view/clash_converter/index.htm >/dev/null 2>&1
 
 # Restart LuCI supaya perubahan terus nampak
-/etc/init.d/uhttpd restart
+/etc/init.d/uhttpd restart >/dev/null 2>&1
 
 echo "✅ Clash Converter installed & LuCI restarted."
 # Padam skrip ini sendiri
