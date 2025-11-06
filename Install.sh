b37f74524c26f6cbd3bdb9b91080ba5ca689c365
raducksijaa <ronald.alan77@gmail.com>
2025-11-06 13:46:31 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -8,11 +8,11 @@ mkdir -p /usr/lib/lua/luci/view/clash_converter >/dev/null 2>&1
 
 # Download controller
 wget -O /usr/lib/lua/luci/controller/clash_converter.lua \
-https://raw.githubusercontent.com/Razifadm/ClashConverter/main/usr/lib/lua/luci/controller/clash_converter.lua >/dev/null 2>&1
+https://raw.githubusercontent.com/Razifadm/ClashConverter/OC/usr/lib/lua/luci/controller/clash_converter.lua >/dev/null 2>&1
 
 # Download view
 wget -O /usr/lib/lua/luci/view/clash_converter/index.htm \
-https://raw.githubusercontent.com/Razifadm/ClashConverter/main/usr/lib/lua/luci/view/clash_converter/index.htm >/dev/null 2>&1
+https://raw.githubusercontent.com/Razifadm/ClashConverter/OC/usr/lib/lua/luci/view/clash_converter/index.htm >/dev/null 2>&1
 
 # Restart LuCI supaya perubahan terus nampak
 /etc/init.d/uhttpd restart >/dev/null 2>&1
